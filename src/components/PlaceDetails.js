3bd1e94b4d2c4ef89c01f43762755874d1fa6de4
Guillaume Chau <alphadelta.fg@gmail.com>
2017-09-29 17:10:27 +0200

    Fix #3 & Code style

--- src/components/PlaceDetails.js
+++ src/components/PlaceDetails.js
@@ -9,12 +9,12 @@ export default {
 
 	methods: {
 		createServices () {
-			this.$placeService = new window.google.maps.places.PlacesService(this.$refs.attributions)
+			this.$_placeService = new window.google.maps.places.PlacesService(this.$refs.attributions)
 		},
 
 		update () {
 			this.loading = true
-			this.$placeService.getDetails(this.request, (results, status) => {
+			this.$_placeService.getDetails(this.request, (results, status) => {
 				this.setResults(results, status)
 				this.loading = false
 			})
--- src/options.js
+++ src/options.js
@@ -2,4 +2,5 @@ export function optionMergeStrategies (Vue) {
 	const strats = Vue.config.optionMergeStrategies
 
 	strats.googleMapsReady = strats.created
+	strats.googleMapsPrepare = strats.created
 }
