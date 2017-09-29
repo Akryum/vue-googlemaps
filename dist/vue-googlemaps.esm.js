5a30348c0beab7e427cfb2d0e8722ebdf9518064
Guillaume Chau <alphadelta.fg@gmail.com>
2017-09-30 01:13:04 +0200

    Version bump
    Publish fail

--- dist/vue-googlemaps.esm.js
+++ dist/vue-googlemaps.esm.js
@@ -2234,7 +2234,7 @@ function registerComponents(Vue, prefix) {
 
 var plugin = {
 	// eslint-disable-next-line no-undef
-	version: "0.0.4",
+	version: "0.0.5",
 	install: function install(Vue, options) {
 		var finalOptions = Object.assign({}, {
 			installComponents: true,
